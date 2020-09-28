6d9b9ff5b259c75d7d78f6d4a6e4a046badbafc0
pengshaohua_eclipse <eclipse_pengshaohua@sunyard.com>
2020-09-29 00:40:53 +0800

    first demo

---
+++ src/main/java/com/sunyard/service/LoginService.java
@@ -0,0 +1,10 @@
+package com.sunyard.service;
+
+public class LoginService {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/com/sunyard/test/Test01.java
@@ -0,0 +1,5 @@
+package com.sunyard.test;
+
+public class Test01 {
+
+}
